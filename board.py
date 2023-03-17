9895adfd42b315259b9d6540c208ba3ca1a4f0b0
Manu Labricole <manu_barriol@hotmail.fr>
2023-03-17 15:50:17 +0100

    Correct init case methode

--- board.py
+++ board.py
@@ -1,41 +1,46 @@
-from case import case
-from pion import pion
-from joueur import joueur
-from game import game
+from case import Case
+#from pion import pion
+#from joueur import joueur
+#from game import game
 import numpy as np
 
-class Board :
 
-    def __init__ (self, case, pion) : 
+class Board:
+
+    def __init__(self):
         ListeCase = list()
         ListCaseJouable = list()
         self.set_case_jouable()
         self.initCase()
 
-    def initCase (self, case):
-        x = 0
-        y = 0
-        for jeu in range(64):
-            case.position = (x+1,y)
-            self.ListeCase.append(case)
-            x+=1
-            if x > 8 :
-                x=0
-                y+=1
-                case.position = (x,y)
-                self.ListeCase.append(case)
-                x+=1
+    #def initCase(self, case):
+    #    x = 0
+    #    y = 0
+    #    for jeu in range(64):
+    #        case.position = (x+1, y)
+    #        self.ListeCase.append(case)
+    #        x += 1
+    #        if x > 8:
+    #            x = 0
+    #            y += 1
+    #            case.position = (x, y)
+    #            self.ListeCase.append(case)
+    #            x += 1
+#
+    #    return ListeCase
+    
+    def initCase(self):
+        for i in range(8):
+            for j in range(8):
+                self.ListeCase.append(Case(position=(i,j), isFree=True, isPlayable=False))
 
-        return ListeCase
 
-
-    def get_case_jouable (self):
+    def get_case_jouable(self):
         ListCaseJouable = list()
-        for case in ListeCase :
+        for case in self.ListeCase:
             if case.is_jouable():
                 ListCaseJouable.append(case)
         return ListCaseJouable
-    
-    def set_case_jouable (self) :
-        return self.get_case_jouable
 
+    def set_case_jouable(self):
+        return self.get_case_jouable
--- sand.py
+++ sand.py
@@ -6,6 +6,7 @@ def initCase():
     for i in range(8):
         for j in range(8):
             caseList.append(Case(position=(i,j), isFree=True, isPlayable=False))
+
 initCase()
 for i in caseList:
     print(i)
