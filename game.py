a21c01c3a45b18caabc5afee451fc03c023e340f
Manu Labricole <manu_barriol@hotmail.fr>
2023-03-17 15:17:43 +0100

    call board from game

--- game.py
+++ game.py
@@ -1,12 +1,16 @@
 from case import Case
-#from board import Board
+from board import Board
 
 
 class Game:
     
     def __init__(self):
         self.initGame()
+        self.gameOn = True
         
     
     def initGame(self):
+        board = Board()
+        print("game init")
         
+Game()
