e13550cc8a6f0c0606ae7f1fcb388aa4379d7632
Manu Labricole <manu_barriol@hotmail.fr>
2023-03-20 14:38:33 +0100

    add isChoicePlyable method

--- board.py
+++ board.py
@@ -27,7 +27,7 @@ class Board:
 
         for i in range(4):
             case = self.getCase(positionCase[i])
-            
+
             case.addPion(Pion(colorPion[i]))
             print(case)
 
@@ -39,10 +39,37 @@ class Board:
         for el in self.listeCase:
             if el.isInPosition(position) == True:
                 return el
-        
+
         return None
 
+    # The game will call this board function to check if this choice
+    # has at least one neighbours to be playable
+    def isChoicePlayable(self, choice, joueurColor):
+        if joueurColor == "black":
+            oppositeColor = "white"
+        else:
+            oppositeColor = "black"
+
+        x = choice[0]
+        y = choice[1]
+
+        cases = [
+            self.getCase(x, y-1),
+            self.getCase(x, y+1),
+            self.getCase(x-1, y),
+            self.getCase(x+1, y)
+        ]
+
+        for case in cases:
+            if case is not None:
+                if case.pion.couleur == oppositeColor:
+                    return True
+
+        print("Apparently your choice is not valid, please place your pion around a neighbours")
+        return False
+
     def get_case_jouable(self):
+
         listCaseJouable = list()
         for case in self.listeCase:
             if case.isPlayable():
@@ -55,11 +82,12 @@ class Board:
     def display_game(self):
         c = 0
         liste = self.listeCase
-        #J'ai remis range 8 pour pouvoir énumérer les lignes déso
-        print (f"    0   1   2   3   4   5   6   7 ")
-        for a in range (8) :
-            
-            print (f"  +---+---+---+---+---+---+---+---")
-            print(f"{a} | {liste[c]} | {liste[c+1]} | {liste[c+2]} | {liste[c+3]} | {liste[c+4]} | {liste[c+5]} | {liste[c+6]} | {liste[c+7]} |" )
-                 #pour itérer à travers les 64 cases
-            c+=8
+        # J'ai remis range 8 pour pouvoir énumérer les lignes déso
+        print(f"    0   1   2   3   4   5   6   7 ")
+        for a in range(8):
+
+            print(f"  +---+---+---+---+---+---+---+---")
+            print(
+                f"{a} | {liste[c]} | {liste[c+1]} | {liste[c+2]} | {liste[c+3]} | {liste[c+4]} | {liste[c+5]} | {liste[c+6]} | {liste[c+7]} |")
+            # pour itérer à travers les 64 cases
+            c += 8
